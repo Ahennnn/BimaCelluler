80a7632d926e5b609da1c9c7728f5b264d8dd361
hendra henz <hendrahenz128@gmail.com>
2019-01-11 17:40:26 +0700

    this is success environment setup

---
+++ bimacellulerBackEnd/src/main/java/net/bc/bimacellulerBackEnd/App.java
@@ -0,0 +1,13 @@
+package net.bc.bimacellulerBackEnd;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
